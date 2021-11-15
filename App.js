315fec92dabf57714c03b319347aba5ef6af9d2e
ZsoltJuhasz <juhasz.zsolt98@gmail.com>
2021-11-15 12:11:39 +0100

    Program bovitese

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableHighlight } from 'react-native';
+
+const Employee = (props) => {
+  return (
+    <View>
+      <Text>Név: {props.name}</Text>
+    </View>
+  );
+}
 
 export default function App() {
+  const [num, setNum] = useState(0);
+
+  function handleButton() {
+    /*location.reload();*/
+    setNum(100);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <Text>{num}</Text>
+      <TouchableHighlight
+        style={styles.runButton}
+        onPress={handleButton}
+      >
+        <Text style={styles.runText}>Button</Text>
+      </TouchableHighlight>
+      <Image
+        source={{
+          width: 300,
+          height: 200,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+      <Image
+        style={styles.image}
+        source={{
+          width: 300,
+          height: 200,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+      <Employee name='Para Béla' />
+      <Employee name='Finom Emese' />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +53,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    marginTop: 10,
+  },
+  runButton: {
+    backgroundColor: 'lightgreen',
+    marginTop: 10,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 20,
+  },
+  runText: {
+    color: 'white',
+    paddingLeft: 10,
+    paddingRight: 10,
+    fontSize: 22,
+  }
 });
